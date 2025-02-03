8a80b6ed001419f055ad5e75754a160f69450538
Berkan Serbes <berkan_serbes@hotmail.com>
2025-02-03 23:32:16 +0300

    feat: Add Menu component to page layout with responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import Menu from "@/components/menu";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
+    <div className="h-screen flex flex-row">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+        <Menu />
+      </div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] flex flex-col">
+        <Navbar />
+      </div>
     </div>
   );
 }
