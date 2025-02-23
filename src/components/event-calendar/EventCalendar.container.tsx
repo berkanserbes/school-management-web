c279e2bb4020f8b7b9d9222f414b4b8c6c34ff91
Berkan Serbes <berkan_serbes@hotmail.com>
2025-02-23 15:51:44 +0300

    feat: Implement EventCalendar component with react-calendar

--- src/components/event-calendar/EventCalendar.container.tsx
+++ src/components/event-calendar/EventCalendar.container.tsx
@@ -1,8 +1,12 @@
+"use client";
+import { useState } from "react";
 import EventCalendarUI from "./EventCalendar.ui";
+import { Value } from "./EventCalendar.type";
 
 const EventCalendarContainer = () => {
+  const [value, onChange] = useState<Value>(new Date());
 
-    return <EventCalendarUI />
-}
+  return <EventCalendarUI value={value} onChange={onChange} />;
+};
 
 export default EventCalendarContainer;
--- src/components/event-calendar/EventCalendar.ui.tsx
+++ src/components/event-calendar/EventCalendar.ui.tsx
@@ -1,8 +1,18 @@
+import Calendar from "react-calendar";
+import { Value } from "./EventCalendar.type";
 
-const EventCalendarUI = () => {
-    return <div>
-        Event Calendar
+const EventCalendarUI = ({
+  value,
+  onChange,
+}: {
+  value: Value;
+  onChange: (value: Value) => void;
+}) => {
+  return (
+    <div>
+      <Calendar value={value} onChange={onChange} />
     </div>
-}
+  );
+};
 
 export default EventCalendarUI;
