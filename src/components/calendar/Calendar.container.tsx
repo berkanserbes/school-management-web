65dbdd6a4599abea1fd04ec1592858a6f819534f
Berkan Serbes <berkan_serbes@hotmail.com>
2025-03-01 14:51:35 +0300

    style: Customize react-big-calendar styles and add view management

--- src/components/calendar/Calendar.container.tsx
+++ src/components/calendar/Calendar.container.tsx
@@ -1,7 +1,25 @@
+"use client";
 import CalendarUI from "./Calendar.ui";
+import moment from "moment";
+import { useState } from "react";
+import { momentLocalizer, View, Views } from "react-big-calendar";
+
+const localizer = momentLocalizer(moment);
 
 const CalendarContainer = () => {
-  return <CalendarUI />;
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleViewOnChange = (selectedView: View) => {
+    setView(selectedView);
+  };
+
+  return (
+    <CalendarUI
+      localizer={localizer}
+      view={view}
+      handleViewOnChange={handleViewOnChange}
+    />
+  );
 };
 
 export default CalendarContainer;
--- src/components/calendar/Calendar.ui.tsx
+++ src/components/calendar/Calendar.ui.tsx
@@ -1,5 +1,28 @@
-const CalendarUI = () => {
-  return <h1>Calendar</h1>;
+"use client";
+import { Calendar, DateLocalizer, View } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { calendarEvents } from "@/lib/data";
+
+const CalendarUI = ({
+  localizer,
+  handleViewOnChange,
+  view,
+}: {
+  localizer: DateLocalizer;
+  handleViewOnChange: (view: View) => void;
+  view: View;
+}) => {
+  return (
+    <Calendar
+      localizer={localizer}
+      onView={handleViewOnChange}
+      startAccessor={"start"}
+      endAccessor={"end"}
+      view={view}
+      events={calendarEvents}
+      views={["work_week", "day"]}
+    />
+  );
 };
 
 export default CalendarUI;
