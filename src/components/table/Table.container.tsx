a46bb8e153c4e218a3fc102926f23858f97b0b24
Berkan Serbes <berkan_serbes@hotmail.com>
2025-03-02 17:55:13 +0300

    feat: Implement generic, reusable Table component with dynamic rendering

--- src/components/table/Table.container.tsx
+++ src/components/table/Table.container.tsx
@@ -1,7 +1,15 @@
-import TableUI from "./Table.ui"
+import TableUI from "./Table.ui";
 
-const TableContainer = () => {
-    return <TableUI />
-}
+const TableContainer = <T extends object>({
+  columns,
+  data,
+  renderRow,
+}: {
+  columns: { header: string; accessor: string; className?: string }[];
+  renderRow: (item: T) => React.ReactNode;
+  data: T[];
+}) => {
+  return <TableUI columns={columns} data={data} renderRow={renderRow} />;
+};
 
 export default TableContainer;
--- src/components/table/Table.ui.tsx
+++ src/components/table/Table.ui.tsx
@@ -1,5 +1,24 @@
-const TableUI = () => {
-  return <h1>TableUI</h1>;
+const TableUI = <T extends object>({
+  columns,
+  renderRow,
+  data,
+}: {
+  columns: { header: string; accessor: string; className?: string }[];
+  renderRow: (item: T) => React.ReactNode;
+  data: T[];
+}) => {
+  return (
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-left text-gray-500 text-sm">
+          {columns.map((column) => (
+            <th key={column.accessor}>{column.header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{data.map((item) => renderRow(item))}</tbody>
+    </table>
+  );
 };
 
 export default TableUI;
